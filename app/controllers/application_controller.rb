e0219c3f40fa035047ce86409b6e23b0dbb8d64b
danielschia <daniel.schia@gmail.com>
2023-06-20 17:33:58 +0100

    Test stimulus, turbo frame, drive and stream

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+  def page1
+    @success_message = "The color has been changed"
+    @reset_message = "The color has been reset"
+  end
+
+  def page2; end
+
+  def page3
+    redirect_to page2_path, status: :see_other
+  end
+
+  def page4; end
+
+  def programming_languages
+    @programming_languages = ProgrammingLanguage.all.reverse_order
+    @programming_language = ProgrammingLanguage.new
+  end
+
+  def create_programming_language
+    @programming_language = ProgrammingLanguage.new params
+                                                    .require(:programming_language)
+                                                    .permit(:name, :release_date, :language)
+    @programming_language.save!
+
+    @counter = ProgrammingLanguage.count
+
+    @programming_language.broadcast_render_to :stream_test,partial: 
+                                              'application/create_programming_language', 
+                                              locals: { 
+                                                counter: 
+                                                ProgrammingLanguage.count 
+                                              }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'page1', to: 'application#page1'
+  get 'page2', to: 'application#page2'
+  post 'page3', to: 'application#page3'
+  get 'page4', to: 'application#page4'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'programming-languages', to: 'application#programming_languages'
+  post 'programming-language', to: 'application#create_programming_language'
 end
